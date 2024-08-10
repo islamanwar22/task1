d779fa169bf078ac9533680a6dc7d66ae28e56c4
islamanwar22 <75851325+islamanwar22@users.noreply.github.com>
2024-08-10 22:32:47 +0300

    Add files via upload

---
+++ problemSolving.js
@@ -0,0 +1,160 @@
+/*
+1- Check if a given number is less than or equal to 100
+If number is less than or equal to 100
+Console.log number is less than or equal to 100
+Else number is greater than 100
+*/
+
+// let x = prompt("PLease enter Number");
+// if (x <= 100) {
+//   console.log("Number is less than or equal to 100");
+// }
+// else{
+//     console.log("Number is greater than 100");
+// }
+
+//----------------------------------------------------------------
+
+/*
+2- Check the number of characters in text
+ If number of characters greater than 100 
+ Console log the string is long 
+ Else console log string is short 
+ */
+
+// let y = prompt("Please enter a text");
+// let number_of_characters = y.length;
+// if (z > 100) {
+//   console.log("the string is long");
+// } else {
+//   console.log("string is short");
+// }
+
+//----------------------------------------------------------------
+
+/*
+ 3- Password validation 
+ If password equal confirm password 
+ Password is validation else no valied 
+ */
+
+// let x = prompt("Please enter a password");
+// let y = prompt("Confirm password");
+// if (x==y){
+//     console.log("Password Match");
+// }
+// else{
+//     console.log("Password Doesn't match");
+// }
+
+//----------------------------------------------------------------
+
+/*
+4- Check the day of the week if saterday 
+It's weekend else work day
+*/
+
+// const d = new Date();
+// let day = d.getDay();
+// if(day == 6){
+//     console.log("It's weekend");
+// }
+// else{
+//     console.log("It's work day");
+// }
+
+//----------------------------------------------------------------
+
+/*
+5- Check number positive or negative
+*/
+
+// let number = prompt("Please enter a number");
+// if (number>=0){
+//     if(number=="")
+//     {
+//         console.log("you didn't enter a number");
+//     }
+//     else if(number==0){
+//         console.log("Number is Zero");
+//     }else{
+//         console.log("Number is positive");
+//     }
+// }else{
+//     console.log("Number is negative");
+// }
+
+//----------------------------------------------------------------
+
+/*
+6- Verifying academic grade less than 50 F
+ Greater 60 it's D 70 C 80 B greater than it A
+*/
+
+//  let grade = prompt("Please enter your grade");
+//  if(grade < 50){
+//     console.log("your grade is F");
+//  }else if(grade >= 50 && grade<70){
+//     console.log("your grade is D");
+//  }else if (grade >= 70 && grade < 80){
+//     console.log("your grade is c");
+//  }else if(grade >= 80 && grade < 90){
+// console.log("your grade is B");
+//  }else if(grade >= 90){
+//     console.log("your grade is A");
+//  }
+
+//----------------------------------------------------------------
+
+/*
+7- Check temperature temp greater than 30 it's hot 
+else it's cold
+*/
+
+// let temperature = prompt("Please enter temperature");
+// if(temperature > 30 ){
+//     console.log("It is hot");
+// }
+// else{
+//     console.log("It is cold");
+// }
+
+//----------------------------------------------------------------
+
+/*
+8- Checking an odd or even number
+*/
+
+// let number = prompt("Please enter a number");
+// if (number % 2 == 0) {
+//   if (number == 0) {
+//     console.log("The number is zero");
+//   } else {
+//     console.log("The number you entered is even");
+//   }
+// } else {
+//   console.log("The number you entered is odd");
+// }
+
+//----------------------------------------------------------------
+
+/*
+9- Checking an odd or even number if greater than 20 
+console log You can enter the university
+Else You can't enter
+*/
+
+// let number = prompt("Please enter a number");
+// if (number % 2 == 0 && number >= 20) {
+//         console.log("The number is greater than 20 and the number is even You can enter the university");
+//     } 
+//     else if(number % 2 == 0 && number < 20){
+//         console.log("The number is less than 20 and the number is even You can't enter the university");
+//     }
+// else if(number % 2 == 1 && number >= 20){
+//     console.log("The number is greater than 20 and the number is odd You can enter the university");
+// }
+// else if (number % 2 == 1 && number < 20){
+//     console.log("The number is less than 20 and the number is odd You can't enter the university");
+// }
+
